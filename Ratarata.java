3c07a57114461dd5ce4ee8eb8479ba69e8b403c6
RiczkyPratama <122728147+RiczkyPratama@users.noreply.github.com>
2023-06-04 07:53:58 +0700

    Main program

---
+++ Ratarata.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.rizuki.kuliah;
+
+/**
+ *
+ * @author furat
+ */
+public class Ratarata {
+    public double nilai1,nilai2,nilaiRata;
+
+    public double getNilai1() {
+        return nilai1;
+    }
+
+    public void setNilai1(double nilai1) {
+        this.nilai1 = nilai1;
+    }
+
+    public double getNilai2() {
+        return nilai2;
+    }
+
+    public void setNilai2(double nilai2) {
+        this.nilai2 = nilai2;
+    }
+
+    public double getNilaiRata() {
+        return ((nilai1+nilai2)/2);
+    }
+    
+}
